f4224110a02644b9044f54d851b172ca40be04a4
Markus Ljunqvist <ljungan95@hotmail.com>
2019-11-15 14:22:10 +0100

    Added and fixed Game2Activity

--- app/src/main/java/com/example/multiplication/Game2Activity.java
+++ app/src/main/java/com/example/multiplication/Game2Activity.java
@@ -3,12 +3,73 @@ package com.example.multiplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.view.View.OnKeyListener;
+
 
 public class Game2Activity extends AppCompatActivity {
+    TextView questionText;
+    Game game;
+    EditText answerTextEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game2);
+        setContentView(R.layout.activity_game1);
+        questionText = (TextView) findViewById(R.id.questionText);
+        answerTextEdit = (EditText) findViewById(R.id.answerTextEdit);
+        answerTextEdit.setOnKeyListener(new OnKeyListener() {
+
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    if(game == null) {
+                        game = new Game(2, Integer.parseInt(answerTextEdit.getText().toString()));
+                    }else{
+                        game.setAnswer(game.getCurrentQuestionPosition() - 1, Integer.parseInt(answerTextEdit.getText().toString()));
+                    }
+                    onResume();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(game != null){
+            answerTextEdit.setHint("Answer");
+            answerTextEdit.getText().clear();
+            questionText.setText(game.nextQuestion());
+        }else{
+            questionText.setText("Välj multiplikationstabell");
+            answerTextEdit.setHint("Specify number");
+        }
+        // Lägg in en if om fråga 20.
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+    }
+
+
+
+
+
 }
